f6e41708669a5a932f2f5baa0452365dd78ee4b8
Alex <aygunin@mts.ru>
2023-06-28 22:58:59 +0300

    fix: remove log

--- src/libs/fetcher.ts
+++ src/libs/fetcher.ts
@@ -1,17 +1,14 @@
 export const fetcher = async (...fetchParams: Parameters<typeof fetch>) => {
 
   const [input, init = {}] = fetchParams
-  console.log('FETCHER', input, init)
   try {
     const response = await fetch(input, {
       mode: 'no-cors',
       ...init
     })
-    const result = await response.text()
-    console.log('result', result)
-    return result
+
+    return response.json()
   } catch (err) {
-    console.log('ERROR', err)
     return Promise.reject(err)
   }
 }
